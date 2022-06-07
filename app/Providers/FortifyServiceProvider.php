de24e44384ac03a3e9e25cc713be36e6bb68f2c9
Martins Greizis <grei0003@hz.nl>
2022-06-07 15:00:07 +0200

    added bootstrap

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -36,6 +36,10 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
+        Fortify::registerView(function() {
+            return view('auth.register');
+        });
+
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,13 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
