e2b4408e83a7425cb14c7c471f52ebbd22ad6c52
Martins Greizis <grei0003@hz.nl>
2022-06-08 13:24:14 +0200

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        // URL::forceScheme('https');
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
